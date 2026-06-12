e00284da71770a1d277552994b633a5f818c4e23
Pawan-k8 <pawan.krishnamurthyy@gmail.com>
2026-06-12 14:41:17 +0530

    Add basic arithmetic functions for addition, subtraction, and multiplication

---
+++ mathematics.py
@@ -0,0 +1,15 @@
+"sum"
+def add(a,b):
+    "sum = a+b"
+    result = a+b
+    return result
+
+def sub(a,b):
+    "sub = a-b"
+    result = a-b
+    return result
+
+def mult(a,b):
+    "mult = a*b"
+    result = a*b
+    return result
